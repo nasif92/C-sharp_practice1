7dda9e70556b66fd6cc4aa8686747fc13fc0f7a5
nasif-92 <nasif.hossain92@gmail.com>
2020-05-04 19:47:56 -0600

    Object creation

--- C-sharp_practice1/Program.cs
+++ C-sharp_practice1/Program.cs
@@ -6,11 +6,15 @@ namespace C_sharp_practice1
     {
         static void Main(string[] args)
         {
+            string name = "Nasif";
             for (int i= 0; i < 5; i++)
             {
                 Console.WriteLine("Hello World!");
 
             }
+            Console.WriteLine("Hello " + name);
+            human x = new human("Nasif");
+            Console.WriteLine("Hello there new object " + x.myName());
         }
     }
 }
---
+++ C-sharp_practice1/humanoid.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class human
+{
+	public String name;
+	public human(String n)
+	{
+		name = n;
+	}
+	public String myName()
+	{
+		return name;
+	}
+}
